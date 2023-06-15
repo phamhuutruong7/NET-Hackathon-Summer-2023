7e61ada256035f4ca619c1a453bffedb21a47e09
Johannes Meesters <johannes.meesters@appsfactory.de>
2023-06-15 10:59:58 +0200

    feat: hook up input field with v-model

--- src/frontend/meatballs/src/components/ZipCodeField.vue
+++ src/frontend/meatballs/src/components/ZipCodeField.vue
@@ -1,19 +1,32 @@
 <script setup>
-import { ref } from 'vue'
-
-const zipCode = ""
 </script>
 
 <template>
   <div class="card">
     <h1>⛅ Meatballs</h1>
-    <h3>Weather recommendations</h3>
+    <h2>Weather based recommendations for entertainment and transportation</h2>
 
-    <input type="text" id="zipcode" name="zipcode" placeholder="ZIP code" minlength="5" maxlength="5" required>
-    <button type="button">Let's go</button>
+    <input type="text" v-model="zipcode" placeholder="ZIP code" minlength="5" maxlength="5" required><br />
+    <button type="button" @click="printZipCode()">Let's go</button>
   </div>
 </template>
 
 <style scoped>
 
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      zipcode: ''
+    };
+  },
+  methods: {
+    printZipCode() {
+      console.log(this.zipcode);
+      // You can do whatever you want with the value here
+    }
+  }
+}
+</script>
