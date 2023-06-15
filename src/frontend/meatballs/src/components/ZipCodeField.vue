dfa847ac054299c4a894c7be6d27ff5aed9a03ea
Johannes Meesters <johannes.meesters@appsfactory.de>
2023-06-15 10:42:24 +0200

    initial frontend

---
+++ src/frontend/meatballs/src/components/ZipCodeField.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const zipCode = ""
+</script>
+
+<template>
+  <div class="card">
+    <h1>⛅ Meatballs</h1>
+    <h3>Weather recommendations</h3>
+
+    <input type="text" id="zipcode" name="zipcode" placeholder="ZIP code" minlength="5" maxlength="5" required>
+    <button type="button">Let's go</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
