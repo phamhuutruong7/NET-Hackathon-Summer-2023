c6c4ff2f1d361d591b230bc0780ade08911e0412
Johannes Meesters <johannes.meesters@appsfactory.de>
2023-06-15 15:36:42 +0200

    New bootstrap frontend

--- src/frontend/meatballs/src/App.vue
+++ src/frontend/meatballs/src/App.vue
@@ -3,20 +3,10 @@ import ZipCodeField from './components/ZipCodeField.vue';
 </script>
 
 <template>
-  <ZipCodeField />
-</template>
+  <div class="container">
+    <h1>⛅ Meatballs</h1>
+    <h3>Weather based recommendations for entertainment and transportation</h3>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <ZipCodeField />
+  </div>
+</template>
---
+++ src/frontend/meatballs/src/components/Recommendation.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { getTransitionRawChildren } from 'vue';
+
+
+defineProps({
+  text: String,
+})
+
+</script>
+
+<template>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">{{ getTitle() }}</h5>
+      <p class="card-text">{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    getTitle() {
+      if (this.text.indexOf(':') > 0 && this.text.indexOf(':') < 30) {
+        return this.text.substr(0, this.text.indexOf(":"))
+      }
+      return ''
+    }
+  }
+}
+// export default {
+//   data() {
+//     return {
+//       title: getTitle()
+//     };
+//   },
+//   methods: {
+//     getTitle() {
+//       console.log(this.text)
+//     }
+//   }
+// }
+</script>
